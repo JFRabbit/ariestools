2c23cfa0126d081ade17ad1052600fe9dee41a29
JasonZhang <zhangfan@k2data.com.cn>
2019-12-13 16:25:45 +0800

    update version 1.0.5

--- ariestools/__init__.py
+++ ariestools/__init__.py
@@ -3,6 +3,6 @@ from .graphql_query_util import graphql_query
 from .json_util import load_json
 from .path_util import replace_sys_path, get_path_by_relative
 from .yaml_util import load_yaml
-from .time_util import parse, get_local_time, get_cloud_time, get_dt_duration_seconds
+from .time_util import *
 
-__version__ = "1.0.4"
+__version__ = "1.0.5"
