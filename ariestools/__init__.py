9c8f03be9a531fa2d5a1b47d3b55d0810c6a3afd
zhangfan <zhangfan@bizseer.com>
2020-07-16 16:40:52 +0800

    fix beauty_print.py can't import

--- ariestools/__init__.py
+++ ariestools/__init__.py
@@ -10,6 +10,6 @@ from .random_util import *
 from .file_util import write_file, get_all_file_ab_path
 from .color_output import black, red, green, yellow, blue, magenta, cyan
 from tenacity import *
-from beauty_print import *
+from .beauty_print import *
 
-__version__ = "1.7.0"
+__version__ = "1.7.1"
