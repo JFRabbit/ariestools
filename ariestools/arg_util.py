bf99bc3a15f03d8f6dc3976aec4e87354670535e
zhangfan <zhangfan@bizseer.com>
2020-08-05 20:42:39 +0800

    change Arg init param

--- ariestools/arg_util.py
+++ ariestools/arg_util.py
@@ -5,13 +5,22 @@ from typing import List
 class Arg:
     """
     参数
+    name or flags - 一个命名或者一个选项字符串的列表，例如 foo 或 -f, --foo。
+    action - 当参数在命令行中出现时使用的动作基本类型。
+    nargs - 命令行参数应当消耗的数目。
+    const - 被一些 action 和 nargs 选择所需求的常数。
+    default - 当参数未在命令行中出现时使用的值。
+    type - 命令行参数应当被转换成的类型。
+    choices - 可用的参数的容器。
+    required - 此命令行选项是否可省略 （仅选项可用）。
+    help - 一个此选项作用的简单描述。
+    metavar - 在使用方法消息中使用的参数值示例。
+    dest - 被添加到 parse_args() 所返回对象上的属性名。
     """
-    def __init__(self, name, flags, help, default=None, required=False):
-        self.name = name
-        self.flags = flags
-        self.help = help
-        self.default = default
-        self.required = required
+
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
 
 
 class Args:
@@ -31,7 +40,7 @@ class Args:
         parser = argparse.ArgumentParser(prog=self.name, description=self.desc)
 
         for arg in self.args:
-            parser.add_argument(arg.name, arg.flags, help=arg.help, default=arg.default, required=arg.required)
+            parser.add_argument(*arg.args, **arg.kwargs)
 
         return parser.parse_args()
 
@@ -40,10 +49,9 @@ if __name__ == '__main__':
     args = Args(
         name='args-demo',
         args=[
-            Arg('-c', '--config', '配置文件路径', 'config.yaml'),
-            Arg('-f', '--file', 'csv文件路径', required=True),
+            Arg('-c', '--config', help='配置文件路径', default='config.yaml'),
+            Arg('-f', '--file', help='csv文件路径', required=False),
+            Arg('-w', action='store_true')
         ],
         desc='Args Demo desc'
-    )
-
-    args.parse()
+    ).parse()
